65117f7986e10100fd913b05e5b9b6463ec84838
Francesco Carraro <francesco.carraro@studenti.unitn.it>
2021-12-30 21:18:29 +0100

    21123021 Hello world

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js - ProgettoIng2 - Carraro Francesco
+
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function (req, res) {
+res.send('Hello World!');
+});
+
+app.listen(app.get('port'), function () {
+console.log('Sono in ascolto sulla porta ' + app.get('port') );
+});
