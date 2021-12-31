287bb3c3500ba1e04076211feb69b9f1fbfb39b4
Francesco Carraro <francesco.carraro@studenti.unitn.it>
2021-12-31 12:21:01 +0100

    21123111 Hello world Heroku

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var app = express();
 
+app.use('/', express.static('public'));
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
