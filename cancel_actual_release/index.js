8457878fbe7e801bc87438a6e5ebcf2e5edb0bd2
Francesco Carraro <francesco.carraro@studenti.unitn.it>
2022-01-02 11:18:40 +0100

    22010211 Mongoconnection

---
+++ cancel_actual_release/index.js
@@ -0,0 +1,20 @@
+// index.js - ProgettoIng2 - Carraro Francesco
+
+//var app = express();
+var express = require('express');
+
+
+
+// Definisco il file app.js
+const app = require('./app/app.js');
+
+// Definisco la connessione a mongo
+mongodb = require('./app/mongodbconnection.js');
+
+// Puntatore alla home page httop presente in /public
+app.use('/', express.static('public'));
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function () {
+console.log('Sono in ascolto sulla porta ' + app.get('port') );
+});
