f3c33089986dc2ee9414f08bdc1199949ae8ba2e
Francesco Carraro <francesco.carraro@studenti.unitn.it>
2022-01-02 11:22:30 +0100

    22010211 Mongoconnection

---
+++ localdb.js
@@ -0,0 +1,41 @@
+var persone = [
+  {
+    id : 0,
+    nome : "Francesco",
+    cognome : "Carraro",
+    anno: 1977
+  }
+  ,
+  {
+    id : 1,
+    nome : "Silvana",
+    cognome : "Sale",
+    anno: 1973
+  }
+  ,
+  {
+    id : 2,
+    nome : "Massimo",
+    cognome : "Caldera",
+    anno : 1971
+  }
+  ,
+  {
+    id : 3,
+    nome : "Mauro",
+    cognome : "Bonvicin",
+    anno : 1971
+  }
+  ,
+  {
+    id : 4,
+    nome : "Anita",
+    cognome : "Ress",
+    anno : 1979
+  }  
+  ];
+
+
+exports.persone = persone;
+
+
