3a1a2fa5abec38b797e751e4c8f3801f3cf8afbf
Francesco Carraro <francesco.carraro@studenti.unitn.it>
2022-01-03 09:28:34 +0100

    22010300 Eliminazione

--- app/app.js
+++ app/app.js
@@ -82,23 +82,6 @@ app.get("/api/selezionapersona", function (req, res) {
 app.get('/api/elimina/:id', function (req, res) {
   var id = req.params.id;
 
-  var MongoClient = require('mongodb').MongoClient;
-  var url = process.env.MONGODB_URI;
-  
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var myquery = {  name: 'Rohit' };
-    db.collection("students").deleteOne(myquery, function(err, obj) {
-      if (err) throw err;
-      console.log("1 document deleted");
-      db.close();
-    });
-  });  
-
-
-
-
-
 });
 
 
